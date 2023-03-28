b2800531a6ae2c84ee3125cbab654a0a9067257f
Fable <discorddens@gmail.com>
2023-03-28 16:03:36 -0400

    switching staff leave alert to dms

--- events/guild/guildMemberRemove.js
+++ events/guild/guildMemberRemove.js
@@ -8,13 +8,18 @@ module.exports = async (Discord, client, member) => {
         let memberNameDiscrim = member.user.tag;
 
         const notifyRoles = ['761640195413377044', '759255791605383208', '756591038373691606'];
-        const memberRoles = member.roles.cache;
+        const notifyUsers = ['338496408988418048', '219169741447626754', '177953924211408899', '366086664495562764'];
 
+        const memberRoles = member.roles.cache;
         const roleNames = memberRoles.filter((role) => role.id !== member.guild.id).map((role) => role.name).join(', ');
 
         if (memberRoles.find((r) => notifyRoles.includes(r.id))) {
 
-            client.channels.cache.get('781985975844733029').send(`⚠️ **Staff Leave Alert**\n\nA staff member has just left the server (<@${memberID || '???'}> | ${memberNameDiscrim || 'Unknown#0000'}).\n\nRoles: **${roleNames}**`);
+            for (let i = 0; i < notifyUsers.length; i++) {
+
+                await client.users.cache.get(notifyUsers[i]).send(`⚠️ **Staff Leave Alert**\n\nA staff member has just left the server (<@${memberID || '???'}> | ${memberNameDiscrim || 'Unknown#0000'}).\n\nRoles: **${roleNames}**`).catch((err) => { return });
+
+            }
 
         }
 
