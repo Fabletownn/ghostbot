2b8ea1f7661e4ed1cf99fcf531f505a82f052ca4
Fable <discorddens@gmail.com>
2023-04-05 17:35:36 -0400

    removing inappropriate username change detection - native automod now supports this

--- events/guild/userUpdate.js
+++ events/guild/userUpdate.js
@@ -51,12 +51,6 @@ module.exports = async (Discord, client, oldUser, newUser) => {
 
                 });
 
-                if (newUsername.match(/(fa..ot|ni..er|ni..a|卐|卍|adolf|hitler)/gmi)) {
-
-                    client.channels.cache.get('1067061056867938386').send(`⚠️ Potentially **harmful profile** has been detected (username change).\n\nUser: <@${newUser.id}> (${newUser.id})\nUsername: \`${newUsername}\`, previously being named \`${oldUsername}\``);
-
-                }
-
             }
 
         }
