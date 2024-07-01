b0e7a54c19f57ce28a15f15c42dfd4c0f4980ee6
Fabletownn <fable_town@hotmail.com>
2024-07-01 15:57:59 -0400

    allowing tech starter to retry if sent too soon

--- events/guild/threadCreate.js
+++ events/guild/threadCreate.js
@@ -4,13 +4,23 @@
 module.exports = async (Discord, client, thread, newlyCreated) => {
     const techSupport = '1082421799578521620';
 
+    const techStarter = '- Check if your issue is already listed in our <#1041125607599243364> threads!\n- Explain your issue in-depth so we can fully assist.\n- Post screenshots, video, or any kind of media that will help us see the problem you\'re experiencing.\n- Post your player-log file, which will help troubleshoot your issue: `%USERPROFILE%\AppData\LocalLow\Kinetic Games\Phasmophobia\Player.log`\n- Having save file corruption issues? DM <@1043623513669513266>!';
+
     if (newlyCreated) {
         switch (thread.parentId) {
             case techSupport:
-                setTimeout(() => thread.send('- Check if your issue is already listed in our <#1041125607599243364> threads!\n- Explain your issue in-depth so we can fully assist.\n- Post screenshots, video, or any kind of media that will help us see the problem you\'re experiencing.\n- Post your player-log file, which will help troubleshoot your issue: `%USERPROFILE%\AppData\LocalLow\Kinetic Games\Phasmophobia\Player.log`\n- Having save file corruption issues? DM <@1043623513669513266>!'), 1500);
+                postStarter(thread, techStarter, 1500);
                 break;
             default:
                 break;
         }
     }
 };
+
+function postStarter(thread, starter, delay) {
+    setTimeout(() => {
+        thread.send({ content: starter }).catch(() => { // If the bot is faster than the author can post the message, retry
+            setTimeout(() => thread.send({ content: starter }), (delay + 1500));
+        });
+    }, delay);
+}
