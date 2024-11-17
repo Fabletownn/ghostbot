d9a0f29685d078b990ca5a79341a4d706ade3a4d
Fabletownn <fable_town@hotmail.com>
2024-11-17 15:29:14 -0500

    playerlog addition

--- events/guild/threadCreate.js
+++ events/guild/threadCreate.js
@@ -5,7 +5,7 @@ module.exports = async (Discord, client, thread, newlyCreated) => {
     if (newlyCreated) {
         const techSupport = '1082421799578521620';
         const vrTechSupport = '1020011442205900870';
-        const techStarter = '- Check if your issue is already listed in our <#1041125607599243364> threads!\n- Explain your issue in-depth so we can fully assist.\n- Post screenshots, video, or any kind of media that will help us see the problem you\'re experiencing.\n- Post your player-log file, which will help troubleshoot your issue: `%USERPROFILE%\\AppData\\LocalLow\\Kinetic Games\\Phasmophobia\\Player.log`\n- Having save file corruption issues? DM <@1043623513669513266>!';
+        const techStarter = '- Check if your issue is already listed in our <#1041125607599243364> threads!\n- Explain your issue in-depth so we can fully assist.\n- Post screenshots, video, or any kind of media that will help us see the problem you\'re experiencing.\n- **PC Platforms**: Post your player-log file, which will help troubleshoot your issue: `%USERPROFILE%\\AppData\\LocalLow\\Kinetic Games\\Phasmophobia\\Player.log`\n- Having save file corruption issues? DM <@1043623513669513266>!';
 
         switch (thread.parentId) {
             case techSupport:
